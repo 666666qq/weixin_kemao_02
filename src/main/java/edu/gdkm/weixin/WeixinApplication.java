b035243ed6047f52e5bf210d27f4fcf30a3a9a70
不朽梦想 <@DESKTOP-JSTMSI9>
2019-03-26 21:19:23 +0800

    完成简单的微信程序开发

--- src/main/java/edu/gdkm/weixin/WeixinApplication.java
+++ src/main/java/edu/gdkm/weixin/WeixinApplication.java
@@ -3,11 +3,11 @@ package edu.gdkm.weixin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class 微信小程序Application {
+@SpringBootApplication 
+public class WeixinApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(微信小程序Application.class, args);
+		SpringApplication.run(WeixinApplication.class, args);
 	}
 
 }
