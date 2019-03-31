04b3c40ab5df3f5e4ad74f043502dae9daa83b8e
不朽梦想 <@DESKTOP-JSTMSI9>
2019-03-31 22:41:10 +0800

    配置修改

--- src/main/java/edu/gdkm/weixin/controller/MessageReceiverController.java
+++ src/main/java/edu/gdkm/weixin/controller/MessageReceiverController.java
@@ -1,6 +1,10 @@
 package edu.gdkm.weixin.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("kemao_02chenzhipeng/message/receiver")
 public class MessageReceiverController {
+	
+	private static final Logger LOG =LoggerFactory.getLogger(MessageReceiverController.class);
+	
+	
 	@GetMapping
 	public String echo(//
 	@RequestParam("signature") String signature,//
@@ -23,6 +31,14 @@ public class MessageReceiverController {
 	
 
 		return echostr;
+	}
+	@PostMapping
+	public String onMessage(@RequestBody String xml) {
+		//收到消息
+		LOG.trace("收到的消息原文:\n{}\n",xml);
+		return "success";
+		
+		
 	}
 }
 
